20d0439482ddeaf550043f327d2645efa48d1dd0
Nikolay Yarunin <nik.yarunin.dev@gmail.com>
2024-07-14 18:49:29 -0400

    Exercise 6.9

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,14 @@ import { useSelector, useDispatch } from 'react-redux'
 import { incrementVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter === '') {
+      return anecdotes;
+    }
+
+    return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter.toLowerCase()));
+  });
+
   const dispatch = useDispatch()
 
   const vote = (id) => {
